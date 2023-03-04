a6979caf2b0d3cfd2c99755ac59ba5f7f5277e65
MarcLipe <marcos.lima52@fatec.sp.gov.br>
2023-03-03 22:43:38 -0300

    Component Params, Props and Key

--- exercicios/src/index.js
+++ exercicios/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import BomDia from './components/BomDia';
 
-import Primeiro from './components/Primeiro'
+// import Primeiro from './components/Primeiro'
 
 
-ReactDOM.render(<Primeiro/>, document.getElementById('root'))
+ReactDOM.render(<BomDia nome="Marcos Felipe" idade={21}/>, document.getElementById('root'))
